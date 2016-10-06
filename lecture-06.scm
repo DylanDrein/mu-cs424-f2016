1f3621158c34c57456b7286f15ffdd942a8917c5
Barak A. Pearlmutter <barak+git@pearlmutter.net>
2016-10-06 14:41:36 +0100

    local variables work, but cannot nest λs

--- lecture-06.scm
+++ lecture-06.scm
@@ -1,15 +1,15 @@
 ;;; A Meta-Circular Interpreter in Scheme
 
 (define eval_
-  (λ (e)
+  (λ (e env)
     (cond ((number? e) e)
 	  ((boolean? e) e)
-	  ((symbol? e) (lookup-var e))
+	  ((symbol? e) (lookup-var e env))
 	  ;; special forms go here:
 	  ((equal? (car e) 'λ) e)	; (λ VARS BODY)
 	  ((equal? (car e) 'quote) (cadr e))
 	  ;; otherwise must be regular function call
-	  (else (let ((eeoe (map eval_ e)))
+	  (else (let ((eeoe (map (λ (e0) (eval_ e0 env)) e)))
 		  (apply_ (car eeoe) (cdr eeoe)))))))
 
 (define apply_
@@ -17,9 +17,14 @@
     (cond ((procedure? f) (apply f args))
 	  (else (let ((vars (cadr f))
 		      (body (caddr f)))
-		  (eval_ body))))))
+		  (eval_ body (map list vars args)))))))
 
-(define lookup-var (λ (s) (cadr (assoc s global-variable-alist))))
+(define lookup-var
+  (λ (s env)
+    (cadr (or (assoc s env)
+	      (assoc s global-variable-alist)
+	      (error "error: unbound variable" s)))))
+
 (define global-variable-alist
   (list (list 'pi pi)
 	(list 'e (exp 1))
