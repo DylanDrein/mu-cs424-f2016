34b144af43ddb6d94ade9df59fb9452487cd6453
Darren Michael Gunning <DARREN.GUNNING.2014@mumail.ie>
2016-10-19 09:22:00 +0200

    Updated lecture-06

--- lecture-06.scm
+++ lecture-06.scm
@@ -1,7 +1,10 @@
+#lang racket/base
+
 ;;; A Meta-Circular Interpreter in Scheme
+;;; In general more powerful or descriptive languages have smaller interpreters
 
 (define eval_
-  (λ (e env)
+  (λ (e env)		; 'env' is the environment where it was defined
     (cond ((symbol? e) (lookup-var e env))
 	  ((not (list? e)) e) ; non-list non-symbol is self-evaluatory
 	  ;; special forms go here:
@@ -12,6 +15,7 @@
 	   (eval_ (if (eval_ (cadr e) env) (caddr e) (cadddr e)) env))
 	  ((equal? (car e) 'quote) (cadr e))
 	  ;; otherwise must be regular function call
+	  ;; eeoe = evaluate elements of e
 	  (else (let ((eeoe (map (λ (e0) (eval_ e0 env)) e)))
 		  (apply_ (car eeoe) (cdr eeoe)))))))
 
@@ -21,7 +25,7 @@
 
 (define apply_
   (λ (f args)
-    (cond ((procedure? f) (apply f args))
+    (cond ((procedure? f) (apply f args))	; "procedure?" checks if f is a procedure/function
 	  ((equal? (car f) '%closure)
 	   (let ((vars (cadr f))
 		 (body (caddr f))
@@ -31,14 +35,21 @@
 
 (define lookup-var
   (λ (s env)
-    (cadr (or (assoc s env)
+    (cadr (or (assoc s env)			; "assoc" returns the full value of the answer found or false if no answer found
 	      (assoc s global-variable-alist)
 	      (error "error: unbound variable" s)))))
 
 (define global-variable-alist
-  (list (list 'pi pi)
+  (list
+    (list 'pi 'pi) ; Causing error when pi is not quoted => "pi: unbound identifier in module in: pi"
 	(list 'e (exp 1))
-	(list '+ +) (list '* *) (list '- -)
+	(list '+ +)
+    (list '* *)
+    (list '- -)
 	(list 'sin sin)
-	(list 'car car) (list 'cdr cdr) (list 'cons cons) (list 'list list)
-	))
+	(list 'car car)
+    (list 'cdr cdr)
+    (list 'cons cons)
+    (list 'list list)
+   )
+)
